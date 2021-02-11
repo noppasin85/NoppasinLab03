22c072913de33e4fc674d9509de5cbbcedad0eb9
noppasin85 <58221051+noppasin85@users.noreply.github.com>
2021-02-11 22:28:07 +0700

    Initial commit

---
+++ routes/resources/views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+        <title>6135512015</title>
+</head>
+
+<body style="background-color:black">  
+
+        <h2 style="text-align:center ; color:white">Send E-mail to you</h2>
+        <h2 style="text-align:center ; color:white">Make by Thanida Wichaidit</h2>
+        <form style="text-align:center ; color:white" name="form1" method="get" action="/sendbasicemail">  
+                Name: <input type="text" name="name"  placeholder="name"><br><br>
+                E-mail: <input type="email" name="email"  placeholder="email"><br><br>
+                Message : <input type="text" name="message"  placeholder="message"  ><br><br>
+                <input type="submit" name="submit" value="Submit">  
+        </form>
+       
+</body>
+</html>
